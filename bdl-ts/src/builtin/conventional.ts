2118ca5da1b66479ae56912c04b601b42be27a6c
JongChan Choi <jong@chan.moe>
2026-03-01 17:36:50 +0900

    feat(bdl-ts): add IR to runtime defs converter

---
+++ bdl-ts/src/builtin/conventional.ts
@@ -0,0 +1,75 @@
+import type * as ir from "../generated/ir.ts";
+import type { Defs } from "@disjukr/bdl-runtime/data-schema";
+import * as $d from "@disjukr/bdl-runtime/data-schema";
+
+export function irToRuntimeDefs(ir: ir.BdlIr): Defs {
+  const result: Defs = {};
+  for (const [defPath, def] of Object.entries(ir.defs)) {
+    switch (def.type) {
+      case "Custom":
+        $d.defineCustom(
+          defPath,
+          irTypeToRuntimeType(def.originalType),
+          {},
+          result,
+        );
+        break;
+      case "Enum":
+        $d.defineEnum(
+          defPath,
+          new Set(def.items.map((item) => item.attributes.value || item.name)),
+          result,
+        );
+        break;
+      case "Oneof":
+        $d.defineOneof(
+          defPath,
+          def.items.map((item) => irTypeToRuntimeType(item.itemType)),
+          result,
+        );
+        break;
+      case "Struct":
+        $d.defineStruct(
+          defPath,
+          def.fields.map((field) => ({
+            name: field.name,
+            fieldType: irTypeToRuntimeType(field.fieldType),
+            optional: field.optional,
+          })),
+          result,
+        );
+        break;
+      case "Union":
+        $d.defineUnion(
+          defPath,
+          def.attributes.discriminator || "type",
+          Object.fromEntries(
+            def.items.map((item) => [
+              item.name,
+              item.fields.map((field) => ({
+                name: field.name,
+                fieldType: irTypeToRuntimeType(field.fieldType),
+                optional: field.optional,
+              })),
+            ]),
+          ),
+          result,
+        );
+        break;
+      case "Proc":
+        break;
+    }
+  }
+  return result;
+}
+
+function irTypeToRuntimeType(type: ir.Type): $d.Type {
+  switch (type.type) {
+    case "Plain":
+      return $d.p(type.valueTypePath);
+    case "Array":
+      return $d.a(type.valueTypePath);
+    case "Dictionary":
+      return $d.d(type.keyTypePath, type.valueTypePath);
+  }
+}
