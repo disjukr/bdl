efc2e10eff2b23fd1ad34b22c7f3b80d14b2e270
JongChan Choi (Rieul) <jong@chan.moe>
2026-05-26 11:47:11 +0900

    Add VS Code references provider (#69)

--- bdl-vscode/src/main.ts
+++ bdl-vscode/src/main.ts
@@ -2,11 +2,13 @@ import * as vscode from "vscode";
 import { initDefinitions } from "./definitions.ts";
 import { initDiagnostics } from "./diagnostics.ts";
 import { initFormatter } from "./formatter.ts";
+import { initReferences } from "./references.ts";
 import { initVirtualDocuments } from "./virtual-documents.ts";
 
 export function activate(context: vscode.ExtensionContext) {
   initDefinitions(context);
   initDiagnostics(context);
   initFormatter(context);
+  initReferences(context);
   initVirtualDocuments(context);
 }
